3973d7766d0435ee5121ed0921481f30d27fea49
Animesh Mondal <animeshtxt@kgpian.iitkgp.ac.in>
2024-12-13 20:46:01 +0530

    Add: schema validation and error handling

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const listingsSchema = require("./schema.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
@@ -15,6 +18,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
 
 main()
 .then((res) => {
@@ -28,11 +32,24 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 };
 
+const validateListing = (req, res, next) => {
+    let {error} = listingsSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else{
+        next();
+    }
+}
+
+
+
+
 app.get("/",  (req, res) => {
     res.send("Hello I am root");
 });
 
-app.get("/testListing", async (req, res) => {
+app.get("/testListing", wrapAsync( async (req, res) => {
     let sampleListing = new Listing ({
         title: "My New Villa",
         description: "By the beach",
@@ -44,54 +61,74 @@ app.get("/testListing", async (req, res) => {
     await sampleListing.save();
     console.log("sample was saved");
     res.send("Successful testing");
-});
+}));
 
-app.get("/listings", async(req, res) => {
+app.get("/listings", wrapAsync( async(req, res) => {
     let allListings = await Listing.find({});
     res.render("./listings/index.ejs", {allListings});
    
     // console.log(allListings);
-});
+}));
 
 
 app.get("/listings/new", (req, res) => {
     res.render("./listings/new.ejs")
 });
 
-app.get("/listings/:id", async(req, res) => {
+app.get("/listings/:id", wrapAsync( async(req, res) => {
     let{id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/show.ejs", {listing});
-});
+}));
 
-app.post("/listings/new", async (req, res) => {
-    // let {title, description, image, price, country, location} = req.body;
-    // let listing = req.body.listing;
+
+// CREATE ROUTE
+app.post("/listings/new", wrapAsync( async (req, res) => {
+    // console.log(req.body);
+    // if(!req.body.listing){
+    //     throw new ExpressError(400, "Send valid listing data");
+    // }
+    let result = listingsSchema.validate(req.body);
+    console.log(result);
+    if(result.error){
+        throw new ExpressError(400, result.error );
+    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync( async (req, res) => {
     let{id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs" , {listing});
-});
+}));
 
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing, "image.url": req.body.listing["image.url"]});
     res.redirect(`/listings/${id}`);
-});
+}));
 
-app.delete("/listings/:id" , async(req, res) => {
+app.delete("/listings/:id" ,wrapAsync( async(req, res) => {
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-});
+}));
 
-app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page not found !"));
+})
+
+app.use((err, req, res, next)=> {
+    let {statusCode=500, message="something went wrong"} = err;
+    res.status(statusCode).render("listings/error.ejs", {message});
+})
+
+
+app.listen(3000, () => {
+    console.log("server is listening to port 3000");
 });
