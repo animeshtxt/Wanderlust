51e0cce9561059082827430980bacd75edadf8f4
Animesh Mondal <animeshtxt@kgpian.iitkgp.ac.in>
2025-01-26 14:13:24 +0530

    Update: router.route

--- routes/listings.js
+++ routes/listings.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const {listingsSchema, reviewSchema} = require("../schema.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 
 const listingControllers = require("../controllers/listings.js");
@@ -11,20 +8,15 @@ const listingControllers = require("../controllers/listings.js");
 
 router.get("/", wrapAsync(listingControllers.showAllListings));
 
-
-router.get("/new", isLoggedIn,listingControllers.createLisitingForm);
-
-router.get("/:id", wrapAsync( listingControllers.showListing));
-
-
-// CREATE ROUTE
-router.post("/new", isLoggedIn, validateListing, wrapAsync(listingControllers.createListing ));
+router.route("/new")
+.get(isLoggedIn,listingControllers.createLisitingForm)
+.post(isLoggedIn, validateListing, wrapAsync(listingControllers.createListing ));
 
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync( listingControllers.editListingForm));
 
-
-router.put("/:id", wrapAsync(listingControllers.updateListing));
-
-router.delete("/:id", isLoggedIn, isOwner,  wrapAsync(listingControllers.destroyListing));
+router.route("/:id")
+  .get(wrapAsync( listingControllers.showListing))
+  .put(wrapAsync(listingControllers.updateListing))
+  .delete( isLoggedIn, isOwner,  wrapAsync(listingControllers.destroyListing));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,22 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const { route } = require("./listings.js");
-const User = require("../models/user.js");
 const passport = require("passport");
-const passportLocal = require("passport-local");
 const { saveRedirectUrl } = require("../middleware.js");
 const userControllers = require("../controllers/users.js");
 
+router.route("/signup")
+.get(userControllers.renderSignupForm)
+.post(wrapAsync(userControllers.signup));
 
-router.get("/signup", userControllers.renderSignupForm);
-
-router.post("/signup", wrapAsync(userControllers.signup));
-
-router.get("/login", userControllers.renderLoginForm);
-
-router.post("/login", saveRedirectUrl, passport.authenticate('local', {
+router.route("/login")
+ .get(userControllers.renderLoginForm)
+ .post(saveRedirectUrl, passport.    authenticate('local', {
     failureRedirect: "/login",
     failureFlash: true,
     }),
     userControllers.login
-);
+  );
 
 router.get("/logout", userControllers.logout);
 
